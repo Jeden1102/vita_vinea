8953d8d03bab27ec0b49d1bdd6d88f8a02ae4276
Jeden1102 <bum123@o2.pl>
2023-01-15 22:21:09 +0100

    deploy

---
+++ precache-manifest.0804be0f3220cf539570adc442192f6d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7362debb3371ea9eb74f",
+    "url": "/vita_vinea/css/about.61000246.css"
+  },
+  {
+    "revision": "3c88f23485066e9236bc",
+    "url": "/vita_vinea/css/app.a66615cf.css"
+  },
+  {
+    "revision": "3ca5cce4ab52193f2055",
+    "url": "/vita_vinea/css/chunk-vendors.f27b23bd.css"
+  },
+  {
+    "revision": "2ab5f19bc29fb186b70ec846cbb54f2c",
+    "url": "/vita_vinea/img/checklist.2ab5f19b.png"
+  },
+  {
+    "revision": "162602bc0df8c8d4a3da0c9ddf062736",
+    "url": "/vita_vinea/img/close.162602bc.png"
+  },
+  {
+    "revision": "e7be25154dc4c11a5346a8e5bc6f41d9",
+    "url": "/vita_vinea/img/logo-vita.e7be2515.png"
+  },
+  {
+    "revision": "eca3dfd3accab317ea48dfd2f98327bd",
+    "url": "/vita_vinea/img/new-calc.eca3dfd3.png"
+  },
+  {
+    "revision": "cf0a9ad0fe9941015037c5c3a4a14503",
+    "url": "/vita_vinea/img/pattern.cf0a9ad0.webp"
+  },
+  {
+    "revision": "416a55699b1a1fa0299960ee35852f68",
+    "url": "/vita_vinea/index.html"
+  },
+  {
+    "revision": "7362debb3371ea9eb74f",
+    "url": "/vita_vinea/js/about.72f99a7b.js"
+  },
+  {
+    "revision": "3c88f23485066e9236bc",
+    "url": "/vita_vinea/js/app.48c68f38.js"
+  },
+  {
+    "revision": "bc2c92963d2f3e32b321",
+    "url": "/vita_vinea/js/chunk-2d216214.431196c0.js"
+  },
+  {
+    "revision": "bdf600318f831cfde006",
+    "url": "/vita_vinea/js/chunk-b18aa712.5250c41d.js"
+  },
+  {
+    "revision": "3ca5cce4ab52193f2055",
+    "url": "/vita_vinea/js/chunk-vendors.1c2cdda7.js"
+  },
+  {
+    "revision": "2da3c77f71ba2bb74502efc5ca84fccb",
+    "url": "/vita_vinea/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vita_vinea/robots.txt"
+  }
+]);

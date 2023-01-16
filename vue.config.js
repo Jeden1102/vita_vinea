853977ee8a46976534db33617be7b796e4aed0e0
Jeden1102 <bum123@o2.pl>
2023-01-16 17:32:50 +0100

    dwa

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/vita_vinea/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
     pwa: {
         themeColor: "#42b983",
         msTileColor: "#42b983",
